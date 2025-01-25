d0301befe062bc97ba874ce11fdb9df7948d6ee9
Nisha <nishajangid629@gmail.com>
2025-01-25 21:08:18 +0530

    ag-grid added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,23 +3,43 @@ import * as Highcharts from 'highcharts';
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HighchartsChartModule } from 'highcharts-angular';
+import { AgGridAngular } from 'ag-grid-angular'; // Angular Data Grid Component
+import type { ColDef } from 'ag-grid-community'; // Column Definition Type Interface
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, HighchartsChartModule],
+  imports: [CommonModule, HighchartsChartModule, AgGridAngular],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   Highcharts: typeof Highcharts = Highcharts;
-
+  color: any = 'green';
   metrics = [
-    { title: 'Overall Visitors', value: '1,210', subtext: 'Data for 1 month', increment: '+5%' },
-    { title: 'Total Patients', value: '871', subtext: 'As of Jan 20, 2025', increment: '+3%' },
+    {
+      title: 'Overall Visitors',
+      value: '1,210',
+      subtext: 'Data for 1 month',
+      increment: '+5%',
+    },
+    {
+      title: 'Total Patients',
+      value: '871',
+      subtext: 'As of Jan 20, 2025',
+      increment: '+3%',
+    },
     { title: 'Book Appointments', value: '291', subtext: 'As of Jan 20, 2025' }, // No increment
-    { title: 'Room Availability', value: '50/100', subtext: 'As of Jan 20, 2025', increment: '-2%' },
+    {
+      title: 'Room Availability',
+      value: '50/100',
+      subtext: 'As of Jan 20, 2025',
+      increment: '-2%',
+    },
   ];
 
   visitChartOptions: Highcharts.Options = {
@@ -67,6 +87,10 @@ export class DashboardComponent {
     ],
   };
 
+  defaultColDef: ColDef = {
+    flex: 1,
+  };
+
   appointments = [
     {
       name: 'Jonny Wilson',
@@ -92,6 +116,39 @@ export class DashboardComponent {
       symptoms: 'Cardiologist',
       status: 'Cancelled',
     },
+  ];
+
+  // Column Definitions: Defines the columns to be displayed.
+  colDefs: ColDef[] = [
+    {
+      field: 'status',
+      headerName: 'Status',
+      cellRenderer: (params: any) => {
+        if (params.value === 'Confirmed') {
+          this.color = 'green';
+        } else if (params.value === 'Cancelled') {
+          this.color = 'red';
+        } else {
+          this.color = 'orange';
+        }
+        return `
+        <span style="
+          padding: 0.5em 1em;
+          font-size: 12px;
+          font-weight: 500;
+          color: ${this.color};
+          background-color: rgba(0, 123, 255, 0.1);
+          border: 1px solid ${this.color};
+          border-radius: 12px;
+        ">
+          ${params.value}
+        </span>
+      `;
+      },
+    },
+    { field: 'name', headerName: 'Name', filter: 'agTextColumnFilter' },
+    { field: 'date', headerName: 'Date' },
+    { field: 'symptoms', headerName: 'Symptoms' },
   ];
 
   schedule = {
